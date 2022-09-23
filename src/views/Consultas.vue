ccf8490c77aac8d58ce76ce9dfd5d5171b68843e
ycapacho <ycapacho@gmail.com>
2022-09-23 08:19:39 -0500

    cambios

--- src/views/Consultas.vue
+++ src/views/Consultas.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <img src="../assets/images/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
+          <img src="../assets/images/paciente-consulta.png" alt="Image" class="img-fluid">
         </div>
         <div class="col-md-6 contents">
           <div class="row justify-content-center row-custom">
